1c967a0fa39407d103c76173b947a2c833fb8744
Alec Leamas <leamas.alec@gmail.com>
2014-03-28 09:50:33 +0100

    config: hook up new config files in build system.

--- autogen.sh
+++ autogen.sh
@@ -5,3 +5,4 @@ rm -rf aclocal.m4 config.guess config.h.in* config.sub \
 find . -name Makefile.in -exec rm {} \;
 find . -name Makefile -exec rm {} \;
 autoreconf -fi
+(cd contrib/hal/ && ./gen-hal-fdi.pl)
